57dcdb929fd31d0852de72da91a682e5d035b02f
gourav <gouravkumardodia@gmail.com>
2021-12-22 22:59:08 -0800

    23dec

--- 23dec/web23dec/src/App.js
+++ 23dec/web23dec/src/App.js
@@ -1,27 +1,35 @@
 import React from 'react'
-import { First } from './Demo';
-import MyContext from './MyContext';
-// Props Drilling
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
+
+import {Link , BrowserRouter , Routes , Route} from 'react-router-dom'
+
 class App extends React.Component
 {
     constructor()
     {
-      super() 
-      this.state = {
-          title : "Good Morning",
-          num : 125
-      }
+      super()   
     }
     render()
     {     
       return <div>
-          <h1>App Component : {this.state.title} , {this.state.num}</h1> 
-          <button onClick={()=>this.setState({title:'Good Afternoon',num:200})}>Change</button>
-          <hr/>  
-          <MyContext.Provider value={{title:this.state.title,
-                                      num:this.state.num}}>
-              <First/>
-          </MyContext.Provider>
+         <h1>App Component</h1>
+        
+        <BrowserRouter>
+
+            <Link to="/"> <b>Home</b>  </Link> &nbsp;
+            <Link to="/about"> <b>About</b>  </Link>  &nbsp;
+            <Link to="/contact"> <b>Contact</b> </Link>  &nbsp;
+            <hr/>
+
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+            </Routes>
+            
+         </BrowserRouter>
       </div>
     }
 }
---
+++ 23dec/web23dec/src/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Contact extends React.Component
+{
+    render()
+    {
+        return <div>
+            <h2>Contact Component</h2>
+            <img src="/imgs/img3.jpg"/>
+        </div>
+    }
+}
+
