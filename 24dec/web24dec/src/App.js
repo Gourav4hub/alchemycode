3e7a054fc437fa90735b72b99e3fadf23778b866
gourav <gouravkumardodia@gmail.com>
2021-12-24 01:08:01 -0800

    '24dec'

--- 24dec/web24dec/src/App.js
+++ 24dec/web24dec/src/App.js
@@ -5,6 +5,7 @@ import DummyData from './ProductData'
 function App()
 {        
   const [carts,setCart]  = useState([])
+  const [isCartVisible,setIsCartVisible] = useState(false)
 
   const addProductToCart = (pid)=>
   {
@@ -19,11 +20,56 @@ function App()
     }   
   }
 
+  const deleteCart = (pid)=>{
+    setCart(carts.filter(ct=>ct.pid!=pid))
+  }
+  
   return <div className='App'>
     <h1>My Shopping Cart</h1>
     <h4 style={{color:'red',textAlign:'right'}}>
-      <span onClick={()=>console.log(carts)}> Cart : {carts.length} &nbsp;&nbsp;&nbsp; </span>
-    </h4>    
+      <span onClick={()=>setIsCartVisible(true)}> Cart : {carts.reduce((x,ob)=>ob.qty+x,0)} &nbsp;&nbsp;&nbsp; </span>
+    </h4>  
+
+    <div style={{display:isCartVisible?"block":"none"}}>
+      <h3>Cart Details</h3>
+      <table className='table table-striped'>
+        <thead>
+          <tr>
+                            <th>S.No.</th>
+                            <th>Image</th>
+                            <th>Product Name</th>                           
+                            <th>Price</th>
+                            <th>Discount</th>
+                            <th>Quantity</th>
+                            <th>Total Price</th>
+                            <th>Operation</th>
+          </tr>
+        </thead>
+        <tbody>
+          {carts.map((prod, index)=>
+          {
+            return <tr>
+                                <td>{index + 1}</td>
+                                <td><img src={prod.image} /></td>
+                                <td>{prod.name}</td>
+                                <td>{prod.price}</td>
+                                <td>{prod.discount}</td>
+                                <td>{prod.qty}</td>
+                                <td>{(prod.price*prod.qty)-(prod.discount*prod.qty)}</td>
+                                <th>
+                                  <button onClick={()=>deleteCart(prod.pid)} className='btn btn-danger'>Delete</button>
+                                  <br/><br/>
+                                  <button className='btn btn-info'>-</button>
+                                  &nbsp;
+                                  <button className='btn btn-info'>+</button>
+                                </th>
+                    </tr>
+          })}
+        </tbody>
+      </table>
+      <button className='btn btn-primary'  onClick={()=>setIsCartVisible(false)}>Close</button>
+    </div>
+
     <hr/>
 
     <Product addProductToCart={addProductToCart}/>
