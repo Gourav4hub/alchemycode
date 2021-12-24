e45026813ad166e0ace0100a32ee7df2d3491d65
gourav <gouravkumardodia@gmail.com>
2021-12-23 22:31:42 -0800

    '24dec'

--- 24dec/web24dec/src/ProductData.js
+++ 24dec/web24dec/src/ProductData.js
@@ -1,13 +1,13 @@
 export default [
- {name:'Bravia V1',company:'Sony',price:89999,discount:2500,image:'/imgs/tv1.jpg',category:'tv'},
- {name:'OLED P1',company:'Samsung',price:49599,discount:0,image:'/imgs/tv2.jpg',category:'tv'},
- {name:'LG Slim',company:'LG',price:29899,discount:1000,image:'/imgs/tv3.jpg',category:'tv'},
- {name:'OLED P2',company:'Samsung',price:19859,discount:200,image:'/imgs/tv4.jpg',category:'tv'},
+ {pid:101,name:'Bravia V1',company:'Sony',price:89999,discount:2500,image:'/imgs/tv1.jpg',category:'tv'},
+ {pid:102,name:'OLED P1',company:'Samsung',price:49599,discount:0,image:'/imgs/tv2.jpg',category:'tv'},
+ {pid:103,name:'LG Slim',company:'LG',price:29899,discount:1000,image:'/imgs/tv3.jpg',category:'tv'},
+ {pid:104,name:'OLED P2',company:'Samsung',price:19859,discount:200,image:'/imgs/tv4.jpg',category:'tv'},
 
- {name:'Samsung Cool',company:'Samsung',price:45899,discount:2000,image:'/imgs/ac1.jpg',category:'ac'},
- {name:'LG Himalaya',company:'LG',price:59899,discount:0,image:'/imgs/ac2.jpg',category:'ac'},
+ {pid:105,name:'Samsung Cool',company:'Samsung',price:45899,discount:2000,image:'/imgs/ac1.jpg',category:'ac'},
+ {pid:106,name:'LG Himalaya',company:'LG',price:59899,discount:0,image:'/imgs/ac2.jpg',category:'ac'},
 
- {name:'Usha A1',company:'Usha',price:1299,discount:500,image:'/imgs/fan1.jpg',category:'fan'},
- {name:'Bajaj Smart',company:'Bajaj',price:1899,discount:0,image:'/imgs/fan2.jpg',category:'fan'},
- {name:'Orient PSPO',company:'Orient',price:2500,discount:0,image:'/imgs/fan3.jpg',category:'fan'}
+ {pid:107,name:'Usha A1',company:'Usha',price:1299,discount:500,image:'/imgs/fan1.jpg',category:'fan'},
+ {pid:108,name:'Bajaj Smart',company:'Bajaj',price:1899,discount:0,image:'/imgs/fan2.jpg',category:'fan'},
+ {pid:109,name:'Orient PSPO',company:'Orient',price:2500,discount:0,image:'/imgs/fan3.jpg',category:'fan'}
 ]
--- 24dec/web24dec/src/productComponent/Product.js
+++ 24dec/web24dec/src/productComponent/Product.js
@@ -1,37 +1,67 @@
-import {useState} from 'react'
-
+import { useState } from 'react'
+import './Product.css'
 import DummyData from '../ProductData'
 
-function Product()
-{         
-  const [products,setProducts] = useState(DummyData)             
-  
-  return <div className='Product'>
-      <table className='table table-hovered'>
-          <thead>
-              <tr>
-                  <th>S.No.</th>
-                  <th>Image</th>
-                  <th>Product Name</th>
-                  <th>Company</th>
-                  <th>Price</th>
-                  <th>Discount</th>
-              </tr>
-          </thead>
-          <tbody>
-              {products.map((prod,index)=>
-              {
-                return <tr>
-                    <td>{index+1}</td>
-                    <td><img src={prod.image}/></td>
-                    <td>{prod.name}</td>
-                    <td>{prod.company}</td>
-                    <td>{prod.price}</td>
-                    <td>{prod.discount}</td>
-                </tr>
-              })}
-          </tbody>
-      </table>
-  </div>
+function Product() 
+{
+    const [products, setProducts] = useState(DummyData)
+
+    const fetchCategory = (event)=>
+    {
+        var category = event.target.innerHTML.toLowerCase()
+        setProducts(DummyData.filter(prod=>category=='all'?true:prod.category==category))
+    }
+
+    const addCart = (event)=>
+    {
+        var pid = event.target.getAttribute('data-id');        
+    }
+    return <div className='Product'>
+
+        <div className='row'>
+            <div className='col-lg-3 text-center filterdiv'> 
+                <h2>All Category</h2>               
+                <hr/>
+                    <h5 onClick={fetchCategory}>All</h5> <br/>
+                    <h5 onClick={fetchCategory}>TV</h5> <br/>
+                    <h5 onClick={fetchCategory}>AC</h5><br/>
+                    <h5 onClick={fetchCategory}>Fan</h5>    <br/>            
+            </div>
+            <div className='col-lg-9'>
+                <table className='table table-hovered'>
+                    <thead>
+                        <tr>
+                            <th>S.No.</th>
+                            <th>Image</th>
+                            <th>Product Name</th>
+                            <th>Company</th>
+                            <th>Price</th>
+                            <th>Discount</th>
+                            <th>Operation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((prod, index) => {
+                            return <tr>
+                                <td>{index + 1}</td>
+                                <td><img src={prod.image} /></td>
+                                <td>{prod.name}</td>
+                                <td>{prod.company}</td>
+                                <td>{prod.price}</td>
+                                <td>{prod.discount}</td>
+                                <th>
+                                    <button onClick={addCart} 
+                                    data-id={prod.pid}
+                                    className='btn btn-success'>Add Cart</button>
+                                </th>
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+
+    </div>
 }
 export default Product
