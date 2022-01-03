a45729ad58b598965e6cf67050b7d72dcd67f517
gourav <gouravkumardodia@gmail.com>
2022-01-03 04:04:24 -0800

    3jan

--- 3jan/web3jan/src/app/app.component.ts
+++ 3jan/web3jan/src/app/app.component.ts
@@ -10,10 +10,19 @@ export class AppComponent
     public title:String = "Good Afternoon !"
     public names = ["Vikas","Mohan","Shyam","Meena","Rajesh"]
 
+    public students = [
+      {roll:101,name:'Vikas',age:23,marks:343.33},
+      {roll:102,name:'Gopal',age:27,marks:266.77}
+    ]
+
     public change()
     {
       this.title = "Good night !"
     }
 
-    
+    public add(roll:any,name:any,age:any,marks:any)
+    {
+      this.students.push({roll:roll.value,name:name.value,
+        age:age.value,marks:marks.value})
+    }    
 }
