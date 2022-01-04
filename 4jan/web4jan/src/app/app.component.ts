fcd5a3cfc037cb8bc0314aac92a53c25dbb93626
gourav dodiam <gouravkumardodia@gmail.com>
2022-01-04 12:19:15 +0530

    4jan

--- 4jan/web4jan/src/app/app.component.ts
+++ 4jan/web4jan/src/app/app.component.ts
@@ -5,13 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+//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
 export class AppComponent
 {
   public products:any = [];
+  public productIndex:number = 0;
+
+  public deleteProduct(pid:number){
+    this.products = this.products.filter((prod:any)=>prod.pid!=pid)
+  }
 
   public add(name:any,price:any,company:any,image:any)
   {
       this.products.push({
+        pid : ++this.productIndex,
         name:name.value,
         price:price.value*1,
         company:company.value,
