3a955dfda3d504d7738e2e139a42a85687597ea5
gourav dodiam <gouravkumardodia@gmail.com>
2022-01-04 09:44:22 +0530

    4jan

---
+++ 4jan/web4jan/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent
+{
+  public num = 10
+
+  change(){
+    this.num += 1
+  }
+}
