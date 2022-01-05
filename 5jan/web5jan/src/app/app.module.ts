869634d40467ca9e051c919394c7292cbb35ef42
gourav dodiam <gouravkumardodia@gmail.com>
2022-01-05 10:29:21 +0530

    5jan

--- 5jan/web5jan/src/app/app.module.ts
+++ 5jan/web5jan/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {HttpClientModule} from '@angular/common/http'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelpComponent } from './help/help.component';
@@ -16,7 +18,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 5jan/web5jan/src/app/home/home.component.ts
+++ 5jan/web5jan/src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit 
+{
+  public patients:any = [];
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void 
+  {
+    this.http.get("https://todearhemant.pythonanywhere.com/patient/api/patients/").subscribe(data=>{
+      this.patients = data
+    });
+  }
 }
